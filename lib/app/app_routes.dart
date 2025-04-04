a6e18d89c369a617bbbf3d7fed2471845485a973
Nahid <md.nahidhossen1911@gmail.com>
2025-04-04 11:07:28 +0600

    Design Sign Up Screen

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:crafty_bay/feature/auth/ui/screens/sign_in_screen.dart';
+import 'package:crafty_bay/feature/auth/ui/screens/sign_up_screen.dart';
 import 'package:crafty_bay/feature/auth/ui/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,8 @@ class AppRoutes {
       route = const SplashScreen();
     }else if (settings.name == SignInScreen.name) {
       route = const SignInScreen();
+    }else if (settings.name == SignUpScreen.name) {
+      route = const SignUpScreen();
     }
     return MaterialPageRoute(
       builder: (context) {
--- lib/feature/auth/ui/screens/sign_up_screen.dart
+++ lib/feature/auth/ui/screens/sign_up_screen.dart
@@ -1,21 +1,18 @@
-import 'package:crafty_bay/app/app_color.dart';
-import 'package:crafty_bay/feature/auth/ui/screens/sign_up_screen.dart';
 import 'package:crafty_bay/feature/auth/ui/widgets/app_logo.dart';
 import 'package:crafty_bay/feature/core/extensions/app_localization_extension.dart';
 import 'package:crafty_bay/feature/core/extensions/text_theme_extension.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatefulWidget {
-  const SignInScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
 
-  static String name = '/signIn';
+  static String name = '/signUp';
 
   @override
-  State<SignInScreen> createState() => _SignInScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _SignInScreenState extends State<SignInScreen> {
+class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,18 +26,32 @@ class _SignInScreenState extends State<SignInScreen> {
               AppLogo(),
               SizedBox(height: 16),
               Text(
-                context.localization.welcomeback,
+                context.localization.completeProfile,
                 style: context.textTheme.titleLarge,
               ),
               Text(
-                context.localization.plzEnterEmailPass,
+                context.localization.signUpdetails,
                 style: TextStyle(color: Colors.grey, fontSize: 16),
               ),
               SizedBox(height: 14),
               TextField(
                 textInputAction: TextInputAction.next,
                 decoration: InputDecoration(
-                  hintText: context.localization.email,
+                  hintText: context.localization.firstName,
+                ),
+              ),
+              SizedBox(height: 14),
+              TextField(
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  hintText: context.localization.lastName,
+                ),
+              ),
+              SizedBox(height: 14),
+              TextField(
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  hintText: context.localization.mobile,
                 ),
               ),
               SizedBox(height: 14),
@@ -50,31 +61,21 @@ class _SignInScreenState extends State<SignInScreen> {
                   hintText: context.localization.password,
                 ),
               ),
+              SizedBox(height: 14),
+              TextField(
+                maxLines: 3,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16,vertical: 10),
+                  hintText: context.localization.address,
+                ),
+              ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {},
                 child: Text(
-                  context.localization.signIn,
+                  context.localization.registration,
                   style: TextStyle(color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 16),
-              RichText(
-                text: TextSpan(
-                  text: context.localization.doNotHaveAccount,
-                  style: TextStyle(color: Colors.grey),
-                  children: [
-                    TextSpan(
-                      text: context.localization.signUp,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.themeColor,
-                      ),
-                      recognizer: TapGestureRecognizer()..onTap = () {
-                        Navigator.pushNamed(context, SignUpScreen.name);
-                      },
-                    ),
-                  ],
                 ),
               ),
             ],
